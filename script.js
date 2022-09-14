029fd15f485a2cd9750ced4fa018549676966ece
flipkoehler <96984853+flipkoehler@users.noreply.github.com>
2022-09-14 18:34:21 +0200

    erster Commit
    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("switchday");
+
+function switchday () {
+    if (h)
+    button.style.backgroundColor("red");
+}
+
+button.addEventListener("click", switchday);
